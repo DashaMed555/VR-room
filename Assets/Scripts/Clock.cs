0697c2ed68b593fbc24673986157187b29707ede
DashaMed555 <DashaMed555@icloud.com>
2026-03-10 11:41:08 +0700

    feat: add clock

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public GameObject hourHand;
+    public GameObject minuteHand;
+    public GameObject secondHand;
+
+    // Update is called once per frame
+    void Update() {
+        UpdateHand();
+    }
+
+    void UpdateHand() {
+        hourHand.transform.localRotation = Quaternion.Euler((System.DateTime.Now.Hour / 12f) * 360f + 180, 0, 0);
+        minuteHand.transform.localRotation = Quaternion.Euler((System.DateTime.Now.Minute / 60f) * 360f + 90, 0, 0);
+        secondHand.transform.localRotation = Quaternion.Euler((System.DateTime.Now.Second / 60f) * 360f + 90, 0, 0);
+    }
+}
